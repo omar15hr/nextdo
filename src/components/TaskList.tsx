fbb6d4cdf930c582b22f886040ecc1583dd7418d
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-04-30 11:26:47 -0400

    Reordenamiento de los task al completarse

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -10,20 +10,25 @@ export function TaskList() {
     ? tasks.filter((task) => task.priority === filterPriority)
     : tasks;
 
+  const sortedTasks = [...filteredTasks].sort((a, b) => {
+    if (a.completed === b.completed) return 0;
+    return a.completed ? 1 : -1;
+  });
+
   return (
     <div className="space-y-3">
-    <div className="flex justify-between items-center">
-      <h2 className="text-2xl font-bold text-blue-500">Tareas</h2>
-      {tasks.length > 0 && <PriorityFilter />}
-    </div>
-
-    {filteredTasks.length === 0 ? (
-      <div className="text-center py-8 text-gray-500 bg-gray-300 rounded-xl">
-        No hay tareas.
+      <div className="flex justify-between items-center">
+        <h2 className="text-2xl font-bold text-blue-500">Tareas</h2>
+        {tasks.length > 0 && <PriorityFilter />}
       </div>
-    ) : (
-      filteredTasks.map((task) => <TaskCard key={task.id} task={task} />)
-    )}
-  </div>
+
+      {sortedTasks.length === 0 ? (
+        <div className="text-center py-8 text-gray-500 bg-gray-300 rounded-xl">
+          No hay tareas.
+        </div>
+      ) : (
+        sortedTasks.map((task) => <TaskCard key={task.id} task={task} />)
+      )}
+    </div>
   );
 }
--- src/store/store.ts
+++ src/store/store.ts
@@ -64,7 +64,7 @@ export const useTasksStore = create<State>()(
         })),
       // Filters
       filterPriority: "",
-      setFilterPriority: (priority) => set({ filterPriority: priority}),
+      setFilterPriority: (priority) => set({ filterPriority: priority }),
     }),
     {
       name: "task-store",
