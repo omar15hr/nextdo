5f21f188b07a34ac90ea405c3991bdaa778db377
Omar Hernández <127421759+omar15hr@users.noreply.github.com>
2025-05-06 10:19:29 -0400

    añadimos el footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { TaskList } from "./components/TaskList";
 import { TaskOverviewPanel } from "./components/TaskOverviewPanel";
@@ -6,10 +7,13 @@ function App() {
   return (
     <div className="flex flex-col bg-gray-200 min-h-screen">
       <Header />
-      <div className="grid grid-cols-1 gap-10 md:grid-cols-2 md:gap-5 p-5">
-        <TaskOverviewPanel />
-        <TaskList />
-      </div>
+      <main className="flex-1">
+        <div className="grid grid-cols-1 gap-10 md:grid-cols-2 md:gap-5 p-5">
+          <TaskOverviewPanel />
+          <TaskList />
+        </div>
+      </main>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer.tsx
@@ -0,0 +1,38 @@
+import { GithubIcon, LinkedinIcon } from "./Icons";
+
+export function Footer() {
+  return (
+    <footer className="bg-blue-600 text-white py-10">
+      <div className="container mx-auto px-6">
+        <div className="flex flex-col md:flex-row justify-between items-center text-center md:text-left">
+          <div className="mb-6 md:mb-0">
+            <h2 className="text-2xl font-bold">Omar Hernández</h2>
+            <p className="mt-2">Desarrollador Web Full Stack</p>
+          </div>
+
+          <div className="flex space-x-6 mb-6 md:mb-0">
+            <h3 className="font-bold">Social Links</h3>
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300 cursor-pointer"
+            >
+              <LinkedinIcon />
+            </a>
+            <a
+              href="#"
+              className="hover:text-white transition-colors duration-300 cursor-pointer"
+            >
+              <GithubIcon />
+            </a>
+          </div>
+
+          <div className="text-center md:text-right">
+            <p>
+              © {new Date().getFullYear()} - Todos los derechos reservados
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
+}
